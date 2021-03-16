ddfd3abf34b75dc9ddc584c02a91fb900c8b9813
gabrielbustos550 <gabrielbustos550@gmail.com>
2021-03-16 10:58:02 -0600

    hw

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+
+import SearchContainer from './components/SearchContainer';
+
+
+const Home = () => {
+  return <div>
+    <h1></h1>
+    <SearchContainer />
+   
+    
+  </div>
+}
+
+
+
+
+export default Home; 
---
+++ src/components/HelloWorld.jsx
@@ -0,0 +1,10 @@
+
+
+
+const HelloWorld = () => {
+    return <div>
+        hello from hello world
+    </div>
+}
+
+export default HelloWorld;
