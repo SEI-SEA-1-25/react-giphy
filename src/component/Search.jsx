be151f35a243dad062467eac58aab29dbb37e070
kchu01 <chukathy01@gmail.com>
2021-03-15 22:52:58 -0700

    Add Step 4 Search Button

--- src/component/Search.jsx
+++ src/component/Search.jsx
@@ -1,13 +1,33 @@
 import { Component } from 'react'
 
 export default class Search extends Component {
+    handleChange = (event) => {
+        console.log('handle change!')
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+        console.log('searched!')
+    }
+
     render() {
         return (
             <div>
                 <h1>Giphy Search</h1>
                 <p>The true source for all things giphy...</p>
-                <input type="text" />
-                <input type="button" value="search" />
+
+                <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="text"
+                        value={this.query}
+                        onChange={this.handleChange}
+                    />
+                    <input
+                        type="submit"
+                        value="search"
+                    />
+
+                </form>
             </div>
         )
     }
