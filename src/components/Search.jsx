ef0c731abe40ae0d2aba3f2ac401c1d86f28ed3f
AlrightErin27 <erinmontybruce@gmail.com>
2021-03-15 20:40:27 -0700

    YOLO

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,51 +1,44 @@
-//Imports//
-import "../Home.css";
-import glass from "../search.gif";
 import { useState, useEffect } from "react";
+import glass from "../search.gif";
 import axios from "axios";
 
-// console.log(process.env.REACT_APP_API_KEY);
-const GIPHY_URL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}`;
+//Variables//
+const API_URL = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=`;
 
-//Main Function
 const Search = (props) => {
-  //Set Init State
-  const [searchValue, setSearchValue] = useState("");
-  //   const dataToSearch = data.title;
-  //   const inturpSearch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${dataToSearch}&limit=100&offset=0&rating=pg-13&lang=en`;
+  const [query, setQuery] = useState("");
 
-  function handleChange(e) {
-    // console.log(e.target.value);
-    setSearchValue(e.target.value);
-  }
-  console.log(props.setSearchValue);
-  //Fetch API data
+  //Hooks//
+  const searchQuery = () => {
+    // console.log("Searched!");
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    searchQuery();
+  };
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+  //   console.log(`Query: ${query}`);
+  //   console.log(`${API_URL}${query}`);
+  let userSearch = `${API_URL}${query}&limit=100&offset=0&rating=pg-13&lang=en`;
+
   useEffect(() => {
     const fetchData = async () => {
-      const response = await axios.get(GIPHY_URL);
+      const response = await axios.get(userSearch);
       console.log(response.data);
-      //   let dataToSearch =
-      //   const searchedTerm = `${GIPHY_URL}&q=${dataToSearch}&limit=100&offset=0&rating=pg-13&lang=en`;
     };
     fetchData();
-  }, []);
+  });
 
   return (
     <div className="search">
-      <h1>Search Giphy</h1>
-      <h3>
-        <img src={glass} alt="searchGif" className="glass" />
-      </h3>
-      <form className="search">
-        <input value={searchValue} onChange={handleChange} type="text" />
-        <input
-          type="submit"
-          // onClick={callSearchFunction}
-          value="SEARCH"
-        />
+      <form onSubmit={handleSubmit}>
+        <input type="text" onChange={handleChange} value={query} />
+        <input type="submit" />
       </form>
+      <img src={glass} className="glass" alt="searchGif" />
     </div>
   );
 };
-
 export default Search;
